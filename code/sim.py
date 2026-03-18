72f5fb05f31468f8c76646484c6f64fc46616711
Jakub Fleischer <jakub.fleischer03@gmail.com>
2026-03-18 10:11:52 +0100

    solmethings wrong, sim runs to ~1e254

--- code/sim.py
+++ code/sim.py
@@ -1,5 +1,6 @@
 import scipy as sp  #.integrate.solve_ivp for diff. equ
 import numpy as np  #numeric stuff
+import scipy.integrate
 from matplotlib import pyplot as plt    #plotting
 from param import *
 
@@ -12,16 +13,16 @@ def u(t):
 
 def kran_nonlin(t, x):
     i = x[0]
-    xc = x[1]
+    xc = np.clip(x[1], 0, 6)
     dxc = x[2]
-    xb = x[3]
+    xb = np.clip(x[3], 0, 1)
     dxb = x[4]
     alpha = x[5]
     dalpha = x[6]
 
     dx = np.array([u(t)/L - R*i/L,
                    dxc,
-                   dc/mc*(dxc-dxb) - cc/mc*(xc-xb) + np.sin(alpha)*mp*g/mc - i*36,69/mc,
+                   dc/mc*(dxc-dxb) - cc/mc*(xc-xb) + np.sin(alpha)*mp*g/mc - i*36.69/mc,
                    dxb,
                    -dc/mb*(dxc-dxb) - cc/mb*(xc-xb) + db/mb*dxb + cb/mb*xb,
                    dalpha,
@@ -32,3 +33,14 @@ def kran_nonlin(t, x):
 
 
 #test solve
+t = [0, 10]
+x0 = np.array([0, 0, 0, 0, 0, 0, 0])
+sol = scipy.integrate.solve_ivp(kran_nonlin, t, x0)
+
+
+plt.plot(sol.t, sol.y[1], label='xc')
+plt.plot(sol.t, sol.y[3], label='xb')
+
+plt.grid()
+plt.legend()
+plt.show()
