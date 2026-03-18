39599196b198f0c966576bdb4424a1d2c9328e08
Jakub Fleischer <jakub.fleischer03@gmail.com>
2026-03-18 10:02:40 +0100

    first sim test

---
+++ code/param.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+R = 34.63   #ohm
+L = 49.6    #mH
+
+cc = 69     #Federkonstante Riemen
+dc = 50     #Reiobung Riemen Ns/m
+mc = 0.82   #masse chassis, masse motor fehlt
+
+mp = 10     #kg
+g = 9.81    #m/s**2
+
+mb = 10     #kg
+db = 69     #ZU BESTIMMEN
+cb = 69     #AUCH
+
+l = 2       #m
+
+drot = 69   #ZU BESTIMMEN
--- code/sim.py
+++ code/sim.py
@@ -1,1 +1,34 @@
-print("gugugagaga")
+import scipy as sp  #.integrate.solve_ivp for diff. equ
+import numpy as np  #numeric stuff
+from matplotlib import pyplot as plt    #plotting
+from param import *
+
+def u(t):
+#input function
+    if t==0:
+        return 1
+    else:
+        return 0
+
+def kran_nonlin(t, x):
+    i = x[0]
+    xc = x[1]
+    dxc = x[2]
+    xb = x[3]
+    dxb = x[4]
+    alpha = x[5]
+    dalpha = x[6]
+
+    dx = np.array([u(t)/L - R*i/L,
+                   dxc,
+                   dc/mc*(dxc-dxb) - cc/mc*(xc-xb) + np.sin(alpha)*mp*g/mc - i*36,69/mc,
+                   dxb,
+                   -dc/mb*(dxc-dxb) - cc/mb*(xc-xb) + db/mb*dxb + cb/mb*xb,
+                   dalpha,
+                   np.sin(alpha)*g/l**2 + drot*dalpha]).T
+
+
+    return dx
+
+
+#test solve
